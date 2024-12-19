5e5dc64c3f0b993392c5f654ed8a4f291581a767
Md Arsalan Alam <arsalanalam160@gmail.com>
2024-12-19 19:49:25 +0530

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,19 @@
+package com.example.quizgame.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL)
+    private List<Question> questions;
+
+    // Getters and setters
+}
